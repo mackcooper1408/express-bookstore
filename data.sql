5d5788e4772449730efd988e86a01628b07b61a7
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-11-03 15:32:40 -0800

    package updates and database setup config

--- data.sql
+++ data.sql
@@ -1,3 +1,29 @@
+\echo 'Delete and recreate bookstore db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE bookstore;
+CREATE DATABASE bookstore;
+\connect bookstore
+
+CREATE TABLE books (
+  isbn TEXT PRIMARY KEY,
+  title TEXT NOT NULL,
+  amazon_url TEXT NOT NULL,
+  author TEXT NOT NULL,
+  language TEXT NOT NULL,
+  pages INTEGER,
+  publisher TEXT NOT NULL,
+  year INTEGER);
+
+
+
+\echo 'Delete and recreate bookstore_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE bookstore_test;
+CREATE DATABASE bookstore_test;
+\connect bookstore_test
+
 CREATE TABLE books (
   isbn TEXT PRIMARY KEY,
   title TEXT NOT NULL,
